ac605384eea1d80ade5156d65a9e6b70edc9ab94
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-06-21 17:01:27 -0500

    search transactions displaying user specific data

--- group-project/src/components/TripByID.jsx
+++ group-project/src/components/TripByID.jsx
@@ -24,7 +24,6 @@ export default function TripByID(props) {
     const navigate = useNavigate();
 
     useEffect(()=>{
-        // console.log(typeof ID)
         fetch("http://localhost:8080/trips/ID/" + ID, {headers: {Authorization: 'Bearer ' + localStorage.getItem('token')}}).then(res=>res.json()).then((result)=>{setTrip(result);})
         fetch("http://localhost:8080/transactions/searchByTripID?ID=" + ID,{headers: {Authorization: 'Bearer ' + localStorage.getItem('token')}}).then(res=>res.json()).then(result=>{setTransactions(result);})
     },[transactions])
@@ -59,12 +58,6 @@ export default function TripByID(props) {
     setTotalSpent(total);
     console.log(totalSpent)
     })
-
-    // console.log(trip.budget)
-    // useEffect(()=> {
-    //     setTotalBudgeted(trip.budget);
-    //     console.log(totalBudgeted)
-    // })
 
     return (
     <div>
